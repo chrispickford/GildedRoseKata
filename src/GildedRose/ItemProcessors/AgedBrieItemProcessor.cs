53d012eb6e84785798b549e396f911d9575dcba7
Chris Pickford <chris@luminousuk.com>
2025-10-21 10:42:57 +0100

    Implement an ItemProcessor for Aged Brie and strangle existing code

---
+++ src/GildedRose/ItemProcessors/AgedBrieItemProcessor.cs
@@ -0,0 +1,24 @@
+﻿namespace GildedRoseKata.ItemProcessors;
+
+internal class AgedBrieItemProcessor : IItemProcessor
+{
+    public bool CanHandleItem(string itemName) =>
+        itemName.Equals("Aged Brie", StringComparison.Ordinal);
+
+    public void UpdateItem(Item item)
+    {
+        item.SellIn--;
+
+        if (item.Quality >= 50)
+        {
+            return;
+        }
+
+        item.Quality++;
+
+        if (item.SellIn < 0)
+        {
+            item.Quality++;
+        }
+    }
+}
